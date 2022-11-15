2307c6cdb78ea2bf3b7c36da868bedc54064f51d
Surya <surya@thesilverlabs.com>
2022-11-16 00:18:02 +0530

    Desktop Version done

--- pages/api/projects.ts
+++ pages/api/projects.ts
@@ -1,35 +1,48 @@
 import type { NextApiRequest, NextApiResponse } from 'next';
 
-export default function handler(req: NextApiRequest, res: NextApiResponse) {
+interface Project {
+  image: string;
+  name: string;
+  tech: string[];
+}
+
+type props = {
+  data: Project[];
+};
+
+export default function handler(
+  req: NextApiRequest,
+  res: NextApiResponse<props>
+) {
   res.status(200).json({
     data: [
       {
-        image: '/images/1.png',
+        image: '/images/1.webp',
         name: 'DESIGN PORTFOLIO',
         tech: ['HTML', 'CSS'],
       },
       {
-        image: '/images/2.png',
+        image: '/images/2.webp',
         name: 'E-LEARNING LANDING PAGE',
         tech: ['HTML', 'CSS'],
       },
       {
-        image: '/images/3.png',
+        image: '/images/3.webp',
         name: 'TODO WEB APP',
         tech: ['HTML', 'CSS', 'Javascript'],
       },
       {
-        image: '/images/4.png',
+        image: '/images/4.webp',
         name: 'ENTERTAINMENT WEB APP',
         tech: ['HTML', 'CSS', 'Javascript'],
       },
       {
-        image: '/images/5.png',
+        image: '/images/5.webp',
         name: 'MEMORY GAME',
         tech: ['HTML', 'CSS', 'Javascript'],
       },
       {
-        image: '/images/6.png',
+        image: '/images/6.webp',
         name: 'ART GALLERY SHOWCASE',
         tech: ['HTML', 'CSS', 'Javascript'],
       },
